9a6c4d6a16d24f55157f5b142deb09b29dab7173
Gaurav Panday <gauravx.gp@gmail.com>
2020-03-11 15:12:16 +0530

    tictocproject

--- sprint_2/script.js
+++ sprint_2/script.js
@@ -10,23 +10,21 @@ var displayrice = document.getElementById("showremainingrice");
 var displaypulse = document.getElementById("showremainingpulse");
 var displaysugar = document.getElementById("showremainingsugar");
 
+var c;
 var stock = [0, 0, 0];
-
 var bodyElem = document.querySelector("body");
 var sum = 0;
-
 var main = document.getElementById("count");
 
-//storing rice,pulse && sugar
+//Function for storing rice,pulse && sugar
 
 function storerice() {
 	var riceadder = document.getElementById("addrice");
 	var x = document.getElementById("enterrice").value;
-
 	stock[0] = stock[0] + Number(x);
-
-	console.log(stock);
+	x.style.color = "white";
 }
+
 riceadder.addEventListener("click", storerice);
 
 function storepulse() {
@@ -34,8 +32,6 @@ function storepulse() {
 	var y = document.getElementById("enterpulse").value;
 	console.log(y);
 	stock[1] = stock[1] + Number(y);
-
-	console.log(stock);
 }
 pulseadder.addEventListener("click", storepulse);
 
@@ -43,19 +39,15 @@ function storesugar() {
 	var sugaradder = document.getElementById("addsugar");
 	var z = document.getElementById("entersugar").value;
 	stock[2] = stock[2] + Number(z);
-
-	console.log(stock);
 }
 sugaradder.addEventListener("click", storesugar);
 
-//deducting rice pulse && sugar aftersale
+//Function for deducting rice pulse && sugar aftersale
 
 function deduct_rice() {
 	var deductrice = document.getElementById("deductrice");
 	var x = document.getElementById("enterrice").value;
 	stock[0] = stock[0] - Number(x);
-
-	console.log(stock);
 }
 deductrice.addEventListener("click", deduct_rice);
 
@@ -63,8 +55,6 @@ function deduct_pulse() {
 	var deductpulse = document.getElementById("deductpulse");
 	var y = document.getElementById("enterpulse").value;
 	stock[1] = stock[1] - Number(y);
-
-	console.log(stock);
 }
 deductpulse.addEventListener("click", deduct_pulse);
 
@@ -72,12 +62,10 @@ function deduct_sugar() {
 	var deductsugar = document.getElementById("deductsugar");
 	var z = document.getElementById("entersugar").value;
 	stock[2] = stock[2] - Number(z);
-
-	console.log(stock);
 }
 deductsugar.addEventListener("click", deduct_sugar);
 
-// Displaying the current stock values
+// Function for Displaying the current stock values
 
 function display_rice() {
 	var displayrice = document.getElementById("showremainingrice");
@@ -85,11 +73,29 @@ function display_rice() {
 	var displaying1 = document.getElementById("displayingrice");
 	var displaying2 = document.getElementById("displayingpulse");
 	var displaying3 = document.getElementById("displayingsugar");
-	displaying1.innerHTML =
-		"Stock" + " " + "of" + " " + "rice" + " " + "is" + " " + stock[0] + "Kg";
-	displaying2.textContent =
-		"Stock" + " " + "of" + " " + "pulse" + " " + "is" + " " + stock[1] + "Kg";
-	displaying3.textContent =
-		"Stock" + " " + "of" + " " + "sugar" + " " + "is" + " " + stock[2] + "Kg";
+	displaying1.innerHTML = "Stock of Rice is" + " " + stock[0] + " " + "Kg";
+	displaying2.textContent = "Stock of Pulse is" + " " + stock[1] + " " + "Kg";
+	displaying3.textContent = "Stock of Sugar is" + " " + stock[2] + " " + "Kg";
 }
 displayrice.addEventListener("click", display_rice);
+
+//Function for displaying alert if any item falls below 10
+
+// function display_alert() {
+// 	var alert = document.getElementById("alert");
+// 	for (i = 0; i < stock.length; i++) {
+// 		if (stock[i] < 10) {
+// 			c++;
+// 		}
+// 	}
+// 	if (c > 0) {
+// 		alert.innerHTML = "You are running out of stock!!!!!";
+// 		alert.style.color = "red";
+// 		alert.style.fontSize = "20px";
+// 	} else {
+// 		alert.innerHTML = "";
+// 	}
+// 	c = 0;
+// 	display_alert(stock);
+// }
+// main.addEventListener("mouseover", display_alert);
